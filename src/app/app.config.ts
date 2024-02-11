fc5ffe7ca0871a84dea1134b131c9a4c69dabb4f
shmool <shmuelaj@gmail.com>
2024-02-11 15:22:46 +0200

    prettify firebase provider code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,16 @@ import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"timestamp-sj","appId":"1:403380079015:web:adcf0b1c7e9362f142ce82","storageBucket":"timestamp-sj.appspot.com","apiKey":"AIzaSyAWk7w3yszuMjftuGREWou9TK0U1byfc7o","authDomain":"timestamp-sj.firebaseapp.com","messagingSenderId":"403380079015","measurementId":"G-QZGHZ8YTNH"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideFunctions(() => getFunctions()))]
+  providers: [provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId":"timestamp-sj",
+      "appId":"1:403380079015:web:adcf0b1c7e9362f142ce82",
+      "storageBucket":"timestamp-sj.appspot.com",
+      "apiKey":"AIzaSyAWk7w3yszuMjftuGREWou9TK0U1byfc7o",
+      "authDomain":"timestamp-sj.firebaseapp.com",
+      "messagingSenderId":"403380079015","measurementId":"G-QZGHZ8YTNH"
+    }))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions()))]
 };
